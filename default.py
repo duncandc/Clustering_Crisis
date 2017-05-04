ffaabb39b6032f4b90b71c5251bcaf9e0a547402
Duncan Campbell <duncan.alan.campbell@gmail.com>
2017-05-04 01:55:18 -0400

    updates

--- default.py
+++ default.py
@@ -2,6 +2,6 @@ import os
 
 PROJECT_DIRECTORY = os.path.dirname(os.path.abspath(__file__))+'/'
 
-#DATA_DIRECTORY = project_directory + 'data/'
+#DATA_DIRECTORY = PROJECT_DIRECTORY + 'data/'
 DATA_DIRECTORY = '/Volumes/burt/bam_data/'
 
